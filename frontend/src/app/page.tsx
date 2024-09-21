f0493d7a41205a70ea7d292ce5f83e09062031ad
Aashi <aashiaggarwal28552@gmail.com>
2024-09-22 01:31:41 +0530

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import Image from "next/image";
+import styles from "./page.module.css";
+import Banner1 from "@/components/Banner1/Banner1";
+import Banner2 from "@/components/Banner2/Banner2";
+
+export default function Home() {
+  return (
+  
+      <main className={styles.main}>
+        <Banner1 />
+        <Banner2 />
+
+ 
+      </main>
+
+
+  );
+}
---
+++ frontend/src/components/Banner2/Banner2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner2 = () => {
+  return (
+    <div>
+      Bannner2
+    </div>
+  )
+}
+
+export default Banner2
