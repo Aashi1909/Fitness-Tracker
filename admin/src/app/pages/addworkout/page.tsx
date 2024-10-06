9507eb56c8acd69c7f5005e983ff201cabcba5a9
Aashi <aashiaggarwal28552@gmail.com>
2024-10-06 15:25:04 +0530

    form for add workout and add execrisees to workout

--- admin/src/app/pages/addworkout/page.tsx
+++ admin/src/app/pages/addworkout/page.tsx
@@ -62,7 +62,25 @@ const page = () => {
     }
   return (
     <div className='formpage'>
-      
+        <h1 className='title'>Add Workout</h1>
+        <input type='text' name='name' placeholder='Workout Name' value={workout.name} onChange={handleWorkoutChange} />
+        <input type='text' name='description' placeholder='Workout Description' value={workout.description} onChange={(e) =>{
+            setWorkout({...workout, description: e.target.value})
+        }}  />
+        <input type='number' name='durationInMinutes' placeholder='Workout Duration In Minutes' value={workout.durationInMinutes} onChange={handleWorkoutChange} />
+        <input type='file' placeholder='Workout Image' name='imageFile' onChange={(e) => setWorkout({...workout, imageFile: e.target.files![0]})} />
+        <div style={{display: 'flex', flexDirection: 'row' , alignItems: 'center', justifyContent: 'center'}}>
+            <h2 className='title'>Add Exercises to Workout</h2>
+            <input type='text' name='name' placeholder='Exercise Name' value={exercise.name} onChange={handleExerciseChange}/>
+            <textarea  name='description' placeholder='Exercise Description' value={exercise.description} onChange={(e) => setExercise({...exercise, description: e.target.value})}/>
+            <label htmlFor='sets'>Sets</label>
+            <input type='number' placeholder='sets' name='sets' value={exercise.sets} onChange={handleExerciseChange} />
+            <label htmlFor='sets'>Reps</label>
+            <input type='number' placeholder='reps' name='reps' value={exercise.reps} onChange={handleExerciseChange} />
+            <input type='file' placeholder='Exercise Image' name='imageFile' onChange={(e) => setExercise({...exercise, imageFile: e.target.files![0]})} />
+            <button onClick={(e) => {addExerciseToWorkout}}>Add Exercise</button>
+
+        </div>
     </div>
   )
 }
